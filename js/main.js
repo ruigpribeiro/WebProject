d3abd50b4d54eb31b2639b639942561310258a05
Liliana Pereira <soffyita@gmail.com>
2024-07-18 16:45:35 +0100

    Continued making changes to the website project

--- js/main.js
+++ js/main.js
@@ -8,18 +8,44 @@ form.addEventListener('submit', e => {
     form.classList.add('was-validated')
 })
 
-const text = "HELLO WORLD!";
+/* Hello World Landing Page */
+const text = "HELLO, WORLD! ";
 let index = 0;
 
 function typeText() {
     if (index < text.length) {
         document.getElementById("typed-text").innerHTML += text.charAt(index);
         index++;
-        setTimeout(typeText, 150); // Adjust the speed here
+        setTimeout(typeText, 150);
     }
 }
 
-// Start the typing effect when the window loads
 window.onload = () => {
     typeText();
 };
+
+/* Button Scroll to the top */
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+    // Show or hide the button based on scroll position
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) { // Adjust scroll position as needed
+            scrollToTopBtn.classList.add('show');
+            scrollToTopBtn.classList.remove('hide');
+        } else {
+            scrollToTopBtn.classList.add('hide');
+            scrollToTopBtn.classList.remove('show');
+        }
+    });
+
+    // Smooth scrolling to top
+    scrollToTopBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
+
