7f4427af3bd3859651ceadfdb5cbabcbc89b4f73
Rui Ribeiro <ruiguilhermeribeiro@gmail.com>
2024-07-29 09:18:15 +0100

    Removed the scroll to top button and started implementing carousel

--- js/main.js
+++ js/main.js
@@ -14,28 +14,4 @@ window.onload = () => {
     typeText();
 };
 
-/* Button Scroll to the top */
-document.addEventListener('DOMContentLoaded', function() {
-    var scrollToTopBtn = document.querySelector('.scroll-to-top');
-
-    // Show or hide the button based on scroll position
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 300) { // Adjust scroll position as needed
-            scrollToTopBtn.classList.add('show');
-            scrollToTopBtn.classList.remove('hide');
-        } else {
-            scrollToTopBtn.classList.add('hide');
-            scrollToTopBtn.classList.remove('show');
-        }
-    });
-
-    // Smooth scrolling to top
-    scrollToTopBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-});
 
